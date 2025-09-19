7c706d93eac2e7c027cf6b9e7d389de11e77c0d6
Lawrence Kimani <larrykimani01@gmail.com>
2025-09-19 16:25:14 +0300

    A very convoluted Hello world.

--- hello_syscalls/main.c
+++ hello_syscalls/main.c
@@ -1,5 +1,21 @@
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdio.h>
 #include <unistd.h>
-
 int main(){
+    int file_descrptor = open("/home/lawrence/Projects/systems/hello_syscalls/hello_world.txt",O_RDONLY);
+    if(file_descrptor == -1){
+        printf("An error occured!: fd\n");
+        return -1;
+    }
+    const int bytes = 100;
+    char buff[bytes];
+    ssize_t retval = read(file_descrptor,buff,bytes);
+    if(retval == -1){
+        printf("An error occured: read \n");
+        return -1;
+    }
+    printf("%s\n",buff);
     return 0;
 }
